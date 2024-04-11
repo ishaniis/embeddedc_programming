04a13d67e3fea9088b1773d9e031ddd60d0f39af
Ishan Sharma <sharmaishan1508@gmail.com>
2024-04-11 18:40:29 -0400

    In this commit, we've discovered the concept of:
    - Break Statement
    - Continue Statement

---
+++ Concepts/21_break_and_continue_statement.c
@@ -0,0 +1,18 @@
+/*
+Break and Continue statements
+*/
+
+/*
+Use of Break Statement:
+- To bring the program control out of the loop
+- Used inside loops and switch statements
+
+*/
+
+/*
+Continue Statement:
+
+- Used to bring control of the iteration to the next iteration in a loop. 
+- Skips some code inside loop and continues with next iteration
+- Used for a condition so that some part of code/logic can be skipped. 
+*/
---
+++ Concepts/22_prog11_breakandcontinue.c
@@ -0,0 +1,46 @@
+/*
+Author: Ishan S.
+Date: 11th April 2024
+Objective: To work with break and continue statements
+*/
+
+#include <stdio.h>
+
+int main(){
+
+    /*
+    Break Statement demonstration:
+    int i, age;
+
+    for(i=0; i < 10; i++){
+        printf("%d Enter your age: ",i);
+        scanf("%d",&age);
+
+        if (age>10){
+            printf("too old \n");
+            break;
+        }
+    }
+
+    */
+
+   /*
+   Continue statement: 
+
+    int i, age;
+
+    for(i=0;i<10;i++){
+        printf(" %d Enter your age:\t", i);
+        scanf("%d",&age);
+
+        if (age > 10){
+            continue;
+        }
+        printf("Out of the loop \n");
+    }
+
+   */
+    
+   
+    return 0;
+}
