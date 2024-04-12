57258aec6ba96880ccec2e8437493f967f34be57
Ishan Sharma <sharmaishan1508@gmail.com>
2024-04-12 07:15:15 -0400

    In this commit,
    We discussed following concepts:
    - Typecasting demonstration & it's syntax for:
    - - C language
    - - Functional notation

--- Concepts/27_prog13_typecasting.c
+++ Concepts/27_prog13_typecasting.c
@@ -3,3 +3,21 @@ Author: Ishan S.
 Date: 11th April 2024
 Objective: To demonstrate the understanding of the typecasting in a given program
 */
+
+/*
+Important type-casting syntax:
+For C: (float) a
+For Functional notation: float (a)
+*/
+
+#include <stdio.h>
+
+int main(){
+
+    int a = 3;
+    float b = (float) 65.0/6 ;
+
+    printf("The value of a is: %d \t and b is: %f \n",a,b);
+    return 0;
+}
+
